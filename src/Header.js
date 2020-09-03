8b6956cf42888aec86b5c7329ca4092d915d5d94
rikvanvelzen <rikvanvelzen@yahoo.com>
2020-09-03 12:39:39 +0200

    added react router for search-home page navigation

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import MenuOpenSharpIcon from "@material-ui/icons/MenuOpenSharp";
 import SearchIcon from "@material-ui/icons/Search";
@@ -6,23 +6,42 @@ import VideoCallIcon from "@material-ui/icons/VideoCall";
 import AppIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import { Avatar } from "@material-ui/core";
+import { link, Link } from "react-router-dom";
 
 // ES7 snippet for fast function creation with rfce short cut
 function Header() {
+  const [inputSearch, setInputSearch] = useState("");
+
   return (
     <div className="header">
       <div className="header__left">
         <MenuOpenSharpIcon />
-        <img
-          className="header__logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
-          alt=""
-        />
+        {/* Why use LINK instead of an <A> tag? because LINK does NOT refresh the page and instantly 
+            updates the element on the page while <a> reloads the whole page! 
+         */}
+        <Link to="/">
+          <img
+            className="header__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+            alt=""
+          />
+        </Link>
       </div>
 
       <div className="header__input">
-        <input placeholder="Search" type="text" />
-        <SearchIcon className="header__inputButton" />
+        {/* An onChange needs to be added otherwise the useState hook will 
+            always take the default empty string state */}
+        <input
+          onChange={(e) => setInputSearch(e.target.value)}
+          value={inputSearch}
+          placeholder="Search"
+          type="text"
+        />
+        {/* Link is a React Router class that you can use to route to any url within your app
+            inputSearch is the value updated by the useState hook */}
+        <Link to={`/search/${inputSearch}`}>
+          <SearchIcon className="header__inputButton" />
+        </Link>
       </div>
 
       <div className="header__icons">
---
+++ src/SearchPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SearchPage() {
+  return <div className="searchPage">This is the search page!</div>;
+}
+
+export default SearchPage;
